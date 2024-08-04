685419e2d7eebb06bcf2c8c4cba9e2f5e158ad58
MINGJI <deorukhkarshruti232@gmail.com>
2024-08-04 12:31:51 +0530

    Changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@example.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
 
-        $this ->call(JobSeeder::class);
+        $this->call(JobSeeder::class);
     }
 }
